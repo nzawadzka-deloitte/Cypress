21cca3f5a367c4aeb4558355881ac3a86df4c1ff
Natalia Zawadzka <nzawadzka@deloittece.com>
2022-07-28 12:53:51 +0200

    init

---
+++ cypress/e2e/automation_practice/automation_practice.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+import {MainPage} from "../page_objects/main-page"
+
+
+
+
+context('e-shop go to', () => {
+
+  beforeEach(() => {    
+
+    MainPage.openAutomationPracticePage();
+
+  })
+
+
+
+  describe('menu bar', () => {
+
+
+
+    it('should open category: Women', () => {
+
+  
+   
+
+    })
+
+
+
+    it('should open cart page', () => {
+
+   
+ 
+
+     })
+
+     it('log in, ')
+
+
+
+  })
+
+})
