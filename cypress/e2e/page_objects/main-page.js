6d7835314f8f840cbb2fc3d27b83b456dd9c8830
Natalia Zawadzka <nzawadzka@deloittece.com>
2022-07-29 09:07:01 +0200

    log

---
+++ cypress/e2e/page_objects/main-page.js
@@ -0,0 +1,20 @@
+export class MainPage {
+
+   
+    static openAutomationPracticePage(){
+        cy.visit("http://automationpractice.com/index.php");
+    }
+
+    static clickCategory(name){
+        cy.get('#block_top_menu').contains(name).click();
+    }
+
+    static clickShoppingCart(){
+        cy.get('.shopping_cart').contains("Cart").click();
+
+    }
+
+    static clickSignButton(){
+        cy.get(".login").contains("Sign in").click();
+    }
+}
